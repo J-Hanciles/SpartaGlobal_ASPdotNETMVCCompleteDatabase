73826ee403b6b64843b6d3407ccf2c820d8032c7
Jerome Hanciles <jhanciles@spartaglobal.com>
2019-03-15 06:00:30 +0000

    base added

---
+++ GamingMVC/GamingMVC/Controllers/ConsoleGamingController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamingMVC.Controllers
+{
+    public class ConsoleGamingController : Controller
+    {
+        // GET: ConsoleGaming
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // About Console Gaming
+        public ActionResult About()
+        {
+            return View();
+        }
+        // Developers
+        public ActionResult Developers()
+        {
+            return View();
+        }
+        // Consoles
+        public ActionResult Consoles()
+        {
+            return View();
+        }
+        // Games
+        public ActionResult Games()
+        {
+            return View();
+        }
+        // Community
+        public ActionResult Community()
+        {
+            return View();
+        }
+        // Stores
+        public ActionResult Stores()
+        {
+            return View();
+        }
+        // Bioware
+        public ActionResult Bioware()
+        {
+            return View();
+        }
+        // Bungie
+        public ActionResult Bungie()
+        {
+            return View();
+        }
+        // EpicGames
+        public ActionResult EpicGames()
+        {
+            return View();
+        }
+        // Insomniac
+        public ActionResult Insomniac()
+        {
+            return View();
+        }
+        // Industries343
+        public ActionResult Industries343()
+        {
+            return View();
+        }
+        // Sega
+        public ActionResult Sega()
+        {
+            return View();
+        }
+        // Ubisoft
+        public ActionResult Ubisoft()
+        {
+            return View();
+        }
+    }
+}
